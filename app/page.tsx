72f0e672b99f999b7e85360c39f5881b44241300
Nikita Sivukhin <sivukhin@turso.tech>
2024-09-16 11:39:39 +0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
-import { createClient } from "@libsql/client/web";
-
-const url = process.env.TURSO_DATABASE_URL!;
-const authToken = process.env.TURSO_AUTH_TOKEN!;
-
-const client = createClient({ url, authToken });
 
 export default function Home() {
-  client.execute("SELECT 1").then(x => console.info(x));
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
---
+++ app/route.ts
@@ -0,0 +1,11 @@
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+import { createClient } from "@libsql/client/web";
+const url = process.env.TURSO_DATABASE_URL!;
+const authToken = process.env.TURSO_AUTH_TOKEN!;
+ 
+export function GET(request: Request) {
+  const client = createClient({ url, authToken });
+  client.execute("SELECT 1").then(x => console.info(x));
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+}
