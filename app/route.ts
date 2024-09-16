95a90c20628394bebf8517a5b94e6f632f4aab12
Nikita Sivukhin <sivukhin@turso.tech>
2024-09-16 11:42:06 +0400

    wip

--- app/route.ts
+++ app/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@libsql/client/web";
 const url = process.env.TURSO_DATABASE_URL!;
 const authToken = process.env.TURSO_AUTH_TOKEN!;
  
-export function GET(_: Request) {
+export function GET() {
   const client = createClient({ url, authToken });
   client.execute("SELECT 1").then(x => console.info(x));
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
